a808124211178a2ced62e53ba84b100118eb79e6
Rohan <shingaderohan96@gmail.com>
2023-04-29 13:07:31 +0530

    practice

--- 14_Arrays/Binary_Search.java
+++ 14_Arrays/Binary_Search.java
@@ -1,6 +1,45 @@
 public class Binary_Search {
+    public static int binarySearch(int numbers[], int key) {
+        int start = 0, end = numbers.length - 1;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
 
-    public static int binarySearch (int numbers[], int key) {
+            //comaparison
+            if (numbers [mid] == key) {
+                return mid;
+            }
+
+            if (numbers[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1; 
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10,12,14,16};
+        int key = 12;
+
+        System.out.println("index value for "+ key + " is: " + binarySearch(numbers, key));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+public static int binarySearch (int numbers[], int key) {
         int start=0, end = numbers.length-1;
         
         while(start <= end) {
@@ -26,4 +65,5 @@ public class Binary_Search {
 
         System.out.println("Index for key is: "+ binarySearch(numbers, key));
     }
-}
+
+*/
