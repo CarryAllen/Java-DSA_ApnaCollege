31090ceb23b8629e97679518d56d7525e8304cb0
CarryAllen <rohanshingade1095@gmail.com>
2023-05-15 18:48:13 +0530

    max sub array program added

--- 14_Arrays/MaxSubarray.java
+++ 14_Arrays/MaxSubarray.java
@@ -1,8 +1,26 @@
 public class MaxSubarray {
-  public static int MaxSubArray() {
-    
+  public static void MaxSubArray(int numbers[]) {
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+
+    for (int i = 0; i<numbers.length; i++) {
+      int start = i;
+      for (int j=i; j<numbers.length; j++) {
+        int end = j;
+        currSum = 0;
+        for (int k=start; k<=end; k++) {  //print
+          //subarray sum
+          currSum += numbers[k];
+        }
+        if (maxSum < currSum) {
+          maxSum = currSum;
+        }
+      }
+    }
+    System.out.println("Max Sum: " + maxSum);
   }
   public static void main(String[] args) {
-    
+    int numbers[] = {2,4,6,8,10};
+    MaxSubArray(numbers);
   }
 }
