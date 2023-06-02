0fc1188e8e9b81caa1f0409c280d442891ef71b7
CarryAllen <rohanshingade1095@gmail.com>
2023-06-02 19:15:03 +0530

    Trapping Rainwater (Medium level) program

---
+++ 14_Arrays/trappinRainwater.java
@@ -0,0 +1,16 @@
+/*
+
+In this program we will learn what is Auxilary Array ? & how to use it by solving this program
+
+ */
+
+public class trappinRainwater {
+
+  public static void rainwater() {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
